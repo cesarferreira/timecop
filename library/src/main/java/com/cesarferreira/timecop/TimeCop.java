ffadf5e4cc8237cd110f40cf5298eb760473d33a
cesarferreira <cesar.manuel.ferreira@gmail.com>
2016-02-17 10:47:26 +0000

    added stopAndDisplayTimePast

--- library/src/main/java/com/cesarferreira/timecop/TimeCop.java
+++ library/src/main/java/com/cesarferreira/timecop/TimeCop.java
@@ -1,135 +1,140 @@
 package com.cesarferreira.timecop;
 
-
 import android.content.Context;
 
+import android.util.Log;
 import java.util.Calendar;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class TimeCop {
-    private static TimeCop ourInstance;
-    private Context context;
+	private static TimeCop ourInstance;
+	private Context context;
+	private String TIMECOP_TAG = "TIMECOP";
 
-    public static TimeCop getInstance(Context context) {
-        if (ourInstance == null) {
-            ourInstance = new TimeCop();
-            ourInstance.context = context;
-        }
-        return ourInstance;
-    }
+	public static TimeCop getInstance(Context context) {
+		if (ourInstance == null) {
+			ourInstance = new TimeCop();
+			ourInstance.context = context;
+		}
+		return ourInstance;
+	}
 
-    private TimeCop() {
-    }
+	private TimeCop() {
+	}
 
-    private final String TIMER_PREFIX = "TIMER-";
+	private final String TIMER_PREFIX = "TIMER-";
 
+	/**
+	 * Start counting time
+	 *
+	 * @param key the key for the timestamp
+	 * @return the timestamp
+	 */
+	public long start(String key) {
 
-    /**
-     * Start counting time
-     *
-     * @param key the key for the timestamp
-     * @return the timestamp
-     */
-    public long start(String key) {
+		if (key.isEmpty()) {
+			throw new InvalidKeyForTimeStampException();
+		}
 
-        if (key.isEmpty()) {
-            throw new InvalidKeyForTimeStampException();
-        }
+		// current timestamp
+		long timestamp = getCurrentTimeInMilliseconds();
 
-        // current timestamp
-        long timestamp = getCurrentTimeInMilliseconds();
+		// save the timestamp
+		PainlessPrefs.getInstance(ourInstance.context).save(TIMER_PREFIX + key, timestamp);
 
-        // save the timestamp
-        PainlessPrefs.getInstance(ourInstance.context).save(TIMER_PREFIX + key, timestamp);
+		return timestamp;
+	}
 
-        return timestamp;
-    }
+	/**
+	 * Reset the timer
+	 *
+	 * @param key the key for the timestamp
+	 */
+	public void reset(String key) {
+		// delete the key
+		PainlessPrefs.getInstance(context).remove(TIMER_PREFIX + key);
+	}
 
-    /**
-     * Reset the timer
-     *
-     * @param key the key for the timestamp
-     */
-    public void reset(String key) {
-        // delete the key
-        PainlessPrefs.getInstance(context).remove(TIMER_PREFIX + key);
-    }
+	/**
+	 * @param key the key for the timestamp
+	 * @return the difference since the timer started
+	 */
+	public long tick(String key) {
 
-    /**
-     * @param key the key for the timestamp
-     * @return the difference since the timer started
-     */
-    public long tick(String key) {
+		long pastTimestamp = PainlessPrefs.getInstance(context).getLong(TIMER_PREFIX + key, -1);
 
-        long pastTimestamp = PainlessPrefs.getInstance(context).getLong(TIMER_PREFIX + key, -1);
+		if (pastTimestamp == -1) {
+			throw new InvalidKeyForTimeStampException();
+		}
 
-        if (pastTimestamp == -1) {
-            throw new InvalidKeyForTimeStampException();
-        }
+		// current timestamp
+		long timestamp = getCurrentTimeInMilliseconds();
 
-        // current timestamp
-        long timestamp = getCurrentTimeInMilliseconds();
+		// the difference
+		long difference = timestamp - pastTimestamp;
 
-        // the difference
-        long difference = timestamp - pastTimestamp;
+		// invalid timestamp
+		if (difference < 0) {
+			throw new InvalidKeyForTimeStampException();
+		}
 
-        // invalid timestamp
-        if (difference < 0) {
-            throw new InvalidKeyForTimeStampException();
-        }
+		return difference;
+	}
 
-        return difference;
-    }
+	/**
+	 * Stop the timer and deletes the key
+	 *
+	 * @param key the key for the timestamp
+	 * @return return the difference
+	 */
+	public long stop(String key) {
 
-    /**
-     * Stop the timer and deletes the key
-     *
-     * @param key the key for the timestamp
-     * @return return the difference
-     */
-    public long stop(String key) {
+		long pastTimestamp = PainlessPrefs.getInstance(context).getLong(TIMER_PREFIX + key, -1);
 
-        long pastTimestamp = PainlessPrefs.getInstance(context).getLong(TIMER_PREFIX + key, -1);
+		if (pastTimestamp == -1) {
+			throw new InvalidKeyForTimeStampException();
+		}
 
-        if (pastTimestamp == -1) {
-            throw new InvalidKeyForTimeStampException();
-        }
+		// current timestamp
+		long timestamp = getCurrentTimeInMilliseconds();
 
-        // current timestamp
-        long timestamp = getCurrentTimeInMilliseconds();
+		// the difference
+		long difference = timestamp - pastTimestamp;
 
-        // the difference
-        long difference = timestamp - pastTimestamp;
+		if (difference < 0) {
+			throw new InvalidKeyForTimeStampException();
+		}
+		// delete the key
+		PainlessPrefs.getInstance(context).remove(TIMER_PREFIX + key);
 
-        if (difference < 0) {
-            throw new InvalidKeyForTimeStampException();
-        }
-        // delete the key
-        PainlessPrefs.getInstance(context).remove(TIMER_PREFIX + key);
+		return difference;
+	}
 
-        return difference;
-    }
+	public void stopAndDisplayTimePast(String key) {
+		long difference = stop(key);
+		Log.d(TIMECOP_TAG, difference + " ms - forKey: " + key);
+	}
 
-    /**
-     * Resets all the timestamps
-     */
-    public void resetAllTimestamps() {
-        Map<String, ?> allEntries = PainlessPrefs.getInstance(context).getAll();
-        for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
-            if (entry.getKey().startsWith(TIMER_PREFIX)) {
-                PainlessPrefs.getInstance(context).remove(entry.getKey());
-            }
-        }
-    }
+	/**
+	 * Resets all the timestamps
+	 */
+	public void resetAllTimestamps() {
+		Map<String, ?> allEntries = PainlessPrefs.getInstance(context).getAll();
+		for (Map.Entry<String, ?> entry : allEntries.entrySet()) {
+			if (entry.getKey().startsWith(TIMER_PREFIX)) {
+				PainlessPrefs.getInstance(context).remove(entry.getKey());
+			}
+		}
+	}
 
-    /**
-     * get Current time in milliseconds
-     *
-     * @return current time in milliseconds
-     */
-    public static long getCurrentTimeInMilliseconds() {
-        return TimeUnit.MILLISECONDS.toMillis(Calendar.getInstance()
-                .getTimeInMillis());
-    }
+	/**
+	 * get Current time in milliseconds
+	 *
+	 * @return current time in milliseconds
+	 */
+	public long getCurrentTimeInMilliseconds() {
+		return TimeUnit.MILLISECONDS.toMillis(Calendar.getInstance()
+			.getTimeInMillis());
+	}
 }
